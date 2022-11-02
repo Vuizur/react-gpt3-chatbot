28220e3a3f0252ae1531cc9e8f79c5b19827fe49
Vuizur <Vuizur@users.noreply.github.com>
2022-11-02 14:59:34 +0100

    Small fixes on Info page

--- src/Info.tsx
+++ src/Info.tsx
@@ -1,8 +1,6 @@
 // An info page with a link to the github repo
 
-import React from 'react';
 import './App.css';
-import { Layout, Menu, MenuProps } from 'antd';
 
 const Info = () => {
     return (
@@ -10,7 +8,6 @@ const Info = () => {
             <h1>Info</h1>
             <p>The chatbot has been primarily developed to let users learn languages in a more fun way, but due to the usage of OpenAI's API it is also much smarter than other chatbots like Replika (and doesn't emotionally manipulate you to get money from you).</p>
             <p>It even corrects the user's errors (which is also powered by OpenAI)</p>
-            <p>For the conversation and the error correction, OpenAI's GPT-3 API is being used. </p>
             <p>This has been developed as an open source project which is available <a href="https://github.com/Vuizur/react-gpt3-chatbot">here. </a>
                 Contributions of new features and bug fixes are always welcome.</p>
             <p>The chatbot remembers only the last 3 messages you typed. This has been done to save your money. 
